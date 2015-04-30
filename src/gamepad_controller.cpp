2b0f6e328db24de9caa2ff9055f4e2ca7b5db494
Philipp <philipp.schmette@tum.de>
2015-04-30 15:08:23 +0200

    added config

--- src/gamepad_controller.cpp
+++ src/gamepad_controller.cpp
@@ -6,15 +6,21 @@ GamepadController::GamepadController(){
 }
 Gamepad *gp;
 bool GamepadController::initialize(){
+    gp = nullptr;
     GamepadHandler::init(this,datamanager());
     //TODO just for testing
-    gp = GamepadHandler::getGamepad(this,"Xbox",false);
 
+    std::vector<std::string> names = getConfig()->getArray<std::string>("controller");
+    std::string dataChannelName = getConfig()->get<std::string>("dataChannelName", "CONTROLLER_1");
+    for(std::string &name:names){
+        gp = GamepadHandler::getGamepad(this,name,dataChannelName,false);
+        if(gp != nullptr)
+            break;
+    }
     if(gp == nullptr) {
-        logger.error("init") << "gamepad is null";
+        logger.error("init") << "gamepad is not found!";
         return false;
     }
-
     setAsXBoxController(*gp);
 
     return true;
